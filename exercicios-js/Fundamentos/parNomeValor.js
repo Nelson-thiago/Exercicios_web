3aa79b358c6669d710d87d2107348b3903b1246e
Nelson Thiago <nesonthiago2012@gmail.com>
2024-02-10 06:02:41 -0400

    adicionado atividade de objetos2 e par nome/valor

---
+++ exercicios-js/Fundamentos/parNomeValor.js
@@ -0,0 +1,9 @@
+//par nome/valor
+const saudacao = "oba" //  contexto lexico 1
+
+function exec() {
+    const saudacao = ' Falaaaaa' //contexto lexico
+    return saudacao
+}
+
+// objetos sao grupos aninhados de pare nome/valor
